77f92c4b4ce9ce5a23deb689a6d20f3d847cdc99
Abdul Rafique <princeabdulrafique@gmail.com>
2024-05-15 23:00:31 +0500

    Initial Project Setup
    Sets the UI and Library projects up to be in a good spot to start work.

--- PostmanClone.DesktopUI/Dashboard.Designer.cs
+++ PostmanClone.DesktopUI/Dashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PostmanClone.DesktopUI;
 
-partial class Form1
+partial class Dashboard
 {
     /// <summary>
     ///  Required designer variable.
@@ -28,10 +28,16 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        SuspendLayout();
+        // 
+        // Dashboard
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Name = "Dashboard";
+        Text = "Dashboard";
+        ResumeLayout(false);
     }
 
     #endregion
